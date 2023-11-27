62808a8dc4bdbc1bf5aca9fb78d42b0614f7f4b1
ahmet uzun <ovuncuzun@gmail.com>
2023-11-27 20:56:24 +0300

    updating the UI

--- src/app/app-detail/app-detail.component.ts
+++ src/app/app-detail/app-detail.component.ts
@@ -3,15 +3,16 @@ import { Component } from '@angular/core';
 import { getSingleCard } from '../cards';
 import { ActivatedRoute } from '@angular/router';
 import { GradientComponent } from '../components/gradient/gradient.component';
+import { CardComponent } from '../components/card/card.component';
 
 @Component({
   selector: 'app-detail',
   standalone: true,
-  imports: [CommonModule, GradientComponent],
+  imports: [CommonModule, CardComponent, GradientComponent],
   templateUrl: './app-detail.component.html',
 })
 export class AppDetailComponent {
-  app: any = null;
+  appCard: any = null;
 
   constructor(private route: ActivatedRoute) {}
 
@@ -19,7 +20,7 @@ export class AppDetailComponent {
     this.route.paramMap.subscribe((params) => {
       const id = params.get('id');
       if (id) {
-        this.app = getSingleCard(id);
+        this.appCard = getSingleCard(id);
       }
     });
   }
